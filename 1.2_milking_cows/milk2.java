b8583f9925c7e07a0c8210aeaf093f86dd3d78cc
minecoder <ronukray@gmail.com>
2017-05-05 01:12:18 +0000

    Beginning algorithm

--- 1.2_milking_cows/milk2.java
+++ 1.2_milking_cows/milk2.java
@@ -16,12 +16,62 @@ class milk2 {
     StringTokenizer st = new StringTokenizer(f.readLine());
 						  // Get line, break into tokens
 		int n = Integer.parseInt(st.nextToken());
-		int[][] farmerTimes = new int[n][2]; 
-		for(int i = 0; i < n; i++) {
-		  farmerTimes[i][0]= 1;
-		  farmerTimes[i][1]= 2;
+		
+		//create and populate timeline
+		int[][] farmerTimes = new int[n * 2][2];
+		for(int i = 0; i < n * 2; i += 2) {
+		  st = new StringTokenizer(f.readLine());
+		  
+		  farmerTimes[i] = new int[]{Integer.parseInt(st.nextToken()), 1};
+		  farmerTimes[i + 1] = new int[] {Integer.parseInt(st.nextToken()), 0};
 		}
-		//System.out.println(Arrays.deepToString(farmerTimes));
+		
+		//put 'em in order
+		Arrays.sort(farmerTimes, new Comparator<int[]>() {
+      @Override
+      public int compare(int[] o1, int[] o2) {
+          return Integer.compare(o1[0], o2[0]);
+      }
+    });
+
+    //longest milking time
+    int max = 0;
+    int current = 0;
+    int start = 0;
+    boolean stillGoing = false;
+    for(int[] coord : farmerTimes) {
+      if(coord[1] == 1) {
+        if(stillGoing == false) {
+          stillGoing = true;
+          start = coord[0];
+        }
+        else{
+          current = coord[0] - start;
+        }
+        System.out.println(coord);
+      }
+      else {
+        if(current > max) {
+          max = current;
+        }
+        current = 0;
+        stillGoing = false;
+      }
+    }
+		
+		System.out.println(Arrays.deepToString(farmerTimes));
+		System.out.println(max);
+		//int[][] farmerTimes = new int[n][2]; 
+		// for(int i = 0; i < n; i++) {
+		//   farmerTimes[i][0]= 1;
+		//   farmerTimes[i][1]= 2;
+		// }
+		
+		// int[] timeline = new int[1000000];
+		// for(int i = 0; i < timeline.length; i++) {
+		//   timeline[i] = i;
+		//   System.out.println(timeline[i]);
+		// }
     //int i1 = Integer.parseInt(st.nextToken());    // first integer
     // int i2 = Integer.parseInt(st.nextToken());    // second integer
     // out.println(i1+i2);                           // output result
