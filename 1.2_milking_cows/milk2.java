8dd96b30eba54f27672383feccd49775fc4d78eb
minecoder <ronukray@gmail.com>
2017-05-06 07:11:51 +0000

    Test #1: First functional one, submitting to USACO

--- 1.2_milking_cows/milk2.java
+++ 1.2_milking_cows/milk2.java
@@ -18,49 +18,127 @@ class milk2 {
 		int n = Integer.parseInt(st.nextToken());
 		
 		//create and populate timeline
-		int[][] farmerTimes = new int[n * 2][2];
-		for(int i = 0; i < n * 2; i += 2) {
+		int[][] farmerTimes = new int[n][2];
+		for(int i = 0; i < n; i++) {
 		  st = new StringTokenizer(f.readLine());
-		  
-		  farmerTimes[i] = new int[]{Integer.parseInt(st.nextToken()), 1};
-		  farmerTimes[i + 1] = new int[] {Integer.parseInt(st.nextToken()), 0};
+		  farmerTimes[i] = new int[]{Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())};
 		}
 		
-		//put 'em in order
-		Arrays.sort(farmerTimes, new Comparator<int[]>() {
-      @Override
-      public int compare(int[] o1, int[] o2) {
-          return Integer.compare(o1[0], o2[0]);
-      }
-    });
-
-    //longest milking time
-    int max = 0;
+		int[] timeline = new int[1000001];
+		int farthestPoint = 0;
+		for(int[] coord : farmerTimes){
+		  for(int i = coord[0]; i <= coord[1]; i++){
+		    timeline[i] = 1;
+		    if(coord[1] > farthestPoint) {
+		      farthestPoint = coord[1]; //determine farthest point for use in idle time
+		    }
+		  }
+		}
+		
+		//System.out.println(farthestPoint);
+		//System.out.println(Arrays.toString(timeline));
+		//System.out.println(timeline[70]);
+		
+		//longest milking time
+		boolean started = false;
     int current = 0;
-    int start = 0;
-    boolean stillGoing = false;
-    for(int[] coord : farmerTimes) {
-      if(coord[1] == 1) {
-        if(stillGoing == false) {
-          stillGoing = true;
-          start = coord[0];
-        }
-        else{
-          current = coord[0] - start;
-        }
-        System.out.println(coord);
-      }
-      else {
-        if(current > max) {
-          max = current;
-        }
-        current = 0;
-        stillGoing = false;
-      }
+    int max = 0;
+    int count = 0;
+    
+    for(int now : timeline) {
+    	if(now == 1) {
+    		if(started == false) { //beginning of chain
+    			started = true;
+    		}
+    		current++;
+    		//System.out.println("on element:"  + count);
+  			//System.out.println(current);
+    	}
+    	if(now == 0 || count == 1000000) { //now = 0
+    		if(started) {//end of chain
+    			started = false;
+    			max = Math.max(max, current);
+    			current = 0;
+    		}
+    	}
+    	count++;
     }
+    
+    int maxMilking = max - 1;
+    
+    System.out.println(maxMilking);
+    //System.out.println(count);
+    
+    
+    //longest idle time
+		started = false;
+    current = 0;
+    max = 0;
+    count = 0;
+    
+    for(int now : timeline) {
+    	if(now == 0) {
+    	  if(count > farthestPoint) {
+    	    break;
+    	  }
+    		if(started == false) { //beginning of chain
+    			started = true;
+    		}
+  			current++;
+  			//System.out.println("on element:"  + count);
+  			//System.out.println(current);
+    	}
+    	if(now == 1) { //now = 0
+    		if(started) {//end of chain
+    			started = false;
+    			max = Math.max(max, current);
+    			current = 0;
+    		}
+    	}
+    	count++;
+    }
+    
+    int maxIdle = max; //counting system on prob text involves non-inclusive, simple subtraction
+    
+    System.out.println(maxIdle);
+    //System.out.println(count);
+    
+
+		//put 'em in order
+		// Arrays.sort(farmerTimes, new Comparator<int[]>() {
+  //     @Override
+  //     public int compare(int[] o1, int[] o2) {
+  //         return Integer.compare(o1[0], o2[0]);
+  //     }
+  //   });
+
+  //   //longest milking time
+  //   int max = 0;
+  //   int current = 0;
+  //   int start = 0;
+  //   boolean stillGoing = false;
+  //   for(int[] coord : farmerTimes) {
+  //     if(coord[1] == 1) {
+  //       if(stillGoing == false) {
+  //         stillGoing = true;
+  //         start = coord[0];
+  //       }
+  //       else{
+  //         current = coord[0] - start;
+  //       }
+  //       System.out.println(coord);
+  //     }
+  //     else {
+  //       if(current > max) {
+  //         max = current;
+  //       }
+  //       current = 0;
+  //       stillGoing = false;
+  //     }
+  //   }
 		
 		System.out.println(Arrays.deepToString(farmerTimes));
-		System.out.println(max);
+		//System.out.println(max);
 		//int[][] farmerTimes = new int[n][2]; 
 		// for(int i = 0; i < n; i++) {
 		//   farmerTimes[i][0]= 1;
@@ -74,7 +152,7 @@ class milk2 {
 		// }
     //int i1 = Integer.parseInt(st.nextToken());    // first integer
     // int i2 = Integer.parseInt(st.nextToken());    // second integer
-    // out.println(i1+i2);                           // output result
-    // out.close();                                  // close the output file
+    out.println(maxMilking + " " + maxIdle);                           // output result
+    out.close();                                  // close the output file
   }
 }
